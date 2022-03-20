4fbf5af492c7fc0f4cfa1bfb50af5a528bfcc8cc
Ayush <ayushkushwaha321@gmail.com>
2022-03-20 09:08:22 +0530

    Get Request Added

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+var db *sql.DB
+
+type studentInfo struct {
+	Sid    string `json:"sid,omitempty"`
+	Name   string `json:"name,omitempty"`
+	Course string `json:"course,omitempty"`
+}
+
+func getMySQLDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:@(127.0.0.1:3306)/go?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func getStudents(w http.ResponseWriter, r *http.Request) {
+	db = getMySQLDB()
+	// _ = db
+	ss := []studentInfo{}
+	s := studentInfo{}
+	rows, err := db.Query("select * from studentinfo")
+	if err != nil {
+		fmt.Fprintf(w, ""+err.Error())
+	} else {
+		for rows.Next() {
+			rows.Scan(&s.Sid, &s.Name, &s.Course)
+			ss = append(ss, s)
+		}
+		json.NewEncoder(w).Encode(ss)
+		// fmt.Fprintf(w, "GET REQUEST: ")
+	}
+}
+func addStudents(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ADD REQUEST")
+}
+func updateStudents(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "UPDATE REQUEST")
+}
+func deleteStudents(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "DELETE REQUEST")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/students", getStudents).Methods("GET")
+	r.HandleFunc("/students", addStudents).Methods("POST")
+	r.HandleFunc("/students/{sid}", updateStudents).Methods("PUT")
+	r.HandleFunc("/students/{sid}", deleteStudents).Methods("DELTE")
+	http.ListenAndServe(":3000", r)
+}
